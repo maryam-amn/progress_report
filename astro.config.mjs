bad4d6f18d4c88c48776772fe3e15dd71ae234ea
maryam-amn <Maryam.Aman@jobtrek.ch>
2025-01-27 08:47:31 +0100

    chore : modify the atro file

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,15 +4,15 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://jobtrek.github.io/', // TODO: Change to your github url
-    base: 'progress-report', // TODO: Change to your repo name
+    site: 'https://maryam-amn.github.io/', // TODO: Change to your github url
+    base: 'progress_report', // TODO: Change to your repo name
     integrations: [starlight({
         title: 'Progress  report',
         social: {
-            github: 'https://github.com/jobtrek/progress-report', // TODO: Change to your repo url
+            github: 'https://github.com/maryam-amn/progress_report', // TODO: Change to your repo url
         },
         editLink: {
-            baseUrl: 'https://github.com/jobtrek/progress-report/edit/main/docs/', // TODO: before /edit, change to your repo url
+            baseUrl: 'https://github.com/maryam-amn/progress_report/edit/main/docs/', // TODO: before /edit, change to your repo url
         },
         customCss: [
             // Relative path to your custom CSS file
